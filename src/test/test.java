0facc6bcd3b03941c8f4d6e7e9d6fe0f10a44aaf
lungjoke <phansak.s@kkumail.com>
2022-10-11 17:05:25 +0700

    Test Case 1 input = 1

--- src/test/test.java
+++ src/test/test.java
@@ -2,13 +2,24 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import lab13.Fizz;
 
 class test {
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		FizzClass fizz;
+		
+		@Test
+		@DisplayName("test input 1")
+		void FIZZBUZZ1() {
+			fizz = new FizzClass(1);
+			assertEquals("1", FizzBuzzCal());
+			
+		}
 	}
 
 }
