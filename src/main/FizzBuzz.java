bd0cd14d2ad285bb6fd5fe875cb7bd9ca3ae2d63
terraexrth <jirapat.ch@kkumail.com>
2022-10-12 15:41:59 +0700

    Test 5 Passed Result = 88

--- src/main/FizzBuzz.java
+++ src/main/FizzBuzz.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class FizzBuzz {
     public static void main(String args[]) {
+        // we should limit an input to not more than 100
         for (int i = 1; i <= 100; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
                 System.out.println("FizzBuzz");
@@ -14,7 +15,6 @@ public class FizzBuzz {
             }else {
                 System.out.println(i);
             }
-
         }
     }
 }
